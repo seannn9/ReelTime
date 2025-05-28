41f5aaa2cb03961a05bc9a9deef6959ff2b418ee
seannn9 <naeskirlu9@gmail.com>
2025-05-28 10:51:59 +0800

    renamed file

--- src/pages/NotFound.jsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-    return (
-        <div>
-            <h1 style={{ fontSize: "3rem" }}>404</h1>
-            <h2>Opps! Page Not Found</h2>
-        </div>
-    );
-}
