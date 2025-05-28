f2d28dcce557362f1c57f6b94f97ccc1048357ce
seannn9 <naeskirlu9@gmail.com>
2025-05-28 10:51:43 +0800

    added a back to home button

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,8 +1,21 @@
+import { useNavigate } from "react-router-dom";
+
 export default function NotFound() {
+    const navigate = useNavigate();
+
     return (
         <div>
-            <h1 style={{ fontSize: "3rem" }}>404</h1>
-            <h2>Opps! Page Not Found</h2>
+            <h1 style={{ fontSize: "4rem" }}>404</h1>
+            <h2>
+                <span className="accent">OOPS!</span> Page Not Found
+            </h2>
+            <button
+                className="welcome-btn primary"
+                onClick={() => navigate("/now-showing-movies")}
+                style={{ margin: "2em" }}
+            >
+                Back to Home
+            </button>
         </div>
     );
 }
