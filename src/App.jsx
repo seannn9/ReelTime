c9da74ecd56041f3167f31f243788a3ac01b3edf
seannn9 <naeskirlu9@gmail.com>
2025-05-29 21:43:30 +0800

    clear auth error after logging out

--- src/App.jsx
+++ src/App.jsx
@@ -57,6 +57,7 @@ export default function App() {
         } else {
             setIsLoggedIn(false);
             setUsername("");
+            setAuthError("");
             navigate("/login");
         }
     };
