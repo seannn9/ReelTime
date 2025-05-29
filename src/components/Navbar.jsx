14afc6e3f06d254512e00f6ceab895d71b2dc64c
seannn9 <naeskirlu9@gmail.com>
2025-05-29 21:44:16 +0800

    added username and log out button *tentative

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import logo from "../assets/favicon32.png";
 import "../styles/components/Navbar.css";
 
-export default function Navbar() {
+export default function Navbar({ username, isLoggedIn, onLogout }) {
     return (
         <nav className="navbar">
             <div className="nav-logo">
@@ -14,7 +14,16 @@ export default function Navbar() {
                     </h1>
                 </Link>
             </div>
-            <div className="nav-links"></div>
+            <div className="nav-links">
+                {isLoggedIn && (
+                    <div className="auth-links">
+                        <p>{username}</p>
+                        <p onClick={onLogout} className="logout-btn">
+                            Logout
+                        </p>
+                    </div>
+                )}
+            </div>
         </nav>
     );
 }
