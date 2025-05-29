9b45be81e740be25adc8345987ae10f986eae4e4
seannn9 <naeskirlu9@gmail.com>
2025-05-29 22:59:12 +0800

    added registration state

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,16 +2,25 @@ import { useState } from "react";
 import "../styles/Login.css";
 import { Link, useNavigate } from "react-router-dom";
 
-export default function Login({ onLogin, authError }) {
+export default function Login({ onLogin, onRegister, authError }) {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
+    const [authState, setAuthState] = useState("login");
 
-    const handleSubmit = async (e) => {
+    const handleLogin = async (e) => {
         e.preventDefault();
         const error = await onLogin(email, password);
         if (!error) {
             navigate("/now-showing-movies");
+        }
+    };
+
+    const handleRegister = async (e) => {
+        e.preventDefault();
+        const error = await onRegister(email, password);
+        if (!error) {
+            setAuthState("login");
         }
     };
 
@@ -24,37 +33,68 @@ export default function Login({ onLogin, authError }) {
                             <Link to="/now-showing-movies">
                                 Reel<span className="accent">Time</span>
                             </Link>{" "}
-                            Login
+                            {authState === "login" ? "Login" : "Register"}
                         </h1>
                         <p>Welcome user! Please enter your details.</p>
                     </div>
                 </div>
-                <form onSubmit={handleSubmit}>
-                    <label htmlFor="email">Email</label>
-                    <input
-                        type="email"
-                        id="email"
-                        placeholder="Enter your email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <label htmlFor="password">Password</label>
-                    <input
-                        type="password"
-                        id="password"
-                        placeholder="Enter your password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    {authError && (
-                        <p style={{ fontStyle: "italic" }}>{authError}</p>
-                    )}
-                    <button type="submit">Log In</button>
-                    <p>
-                        Don't have an account?{" "}
-                        <span className="accent">Register for free!</span>
-                    </p>
-                </form>
+                {authState === "login" ? (
+                    <form onSubmit={handleLogin}>
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            placeholder="Enter your email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            placeholder="Enter your password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        {authError && (
+                            <p style={{ fontStyle: "italic" }}>{authError}</p>
+                        )}
+                        <button type="submit">Log In</button>
+                        <p>
+                            Don't have an account?{" "}
+                            <span onClick={() => setAuthState("register")}>
+                                Register for free!
+                            </span>
+                        </p>
+                    </form>
+                ) : (
+                    authState === "register" && (
+                        <form onSubmit={handleRegister}>
+                            <label htmlFor="email">Email</label>
+                            <input
+                                type="email"
+                                id="email"
+                                placeholder="Enter your email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                            <label htmlFor="password">Password</label>
+                            <input
+                                type="password"
+                                id="password"
+                                placeholder="Enter your password"
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <button type="submit">Register</button>
+                            <p>
+                                Already have an account?{" "}
+                                <span onClick={() => setAuthState("login")}>
+                                    Login now!
+                                </span>
+                            </p>
+                        </form>
+                    )
+                )}
             </section>
         </div>
     );
