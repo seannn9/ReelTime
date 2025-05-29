641c031b51729cd61561aea87cf7bdf6a5592e12
seannn9 <naeskirlu9@gmail.com>
2025-05-29 21:33:14 +0800

    added authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import "./styles/App.css";
 import Welcome from "./pages/Welcome";
 import Home from "./pages/Home";
@@ -6,14 +6,84 @@ import Navbar from "./components/Navbar";
 import ReserveSeat from "./pages/ReserveSeat";
 import NotFound from "./pages/404";
 import Login from "./pages/Login";
+import { useState, useEffect } from "react";
+import supabase from "./server/supabase-client";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
+    const [authError, setAuthError] = useState(null);
+    const [username, setUsername] = useState("");
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const getUser = async () => {
+            const {
+                data: { user },
+                error,
+            } = await supabase.auth.getUser();
+            if (error && error.message !== "Auth session missing!") {
+                setAuthError(error.message);
+            } else {
+                setUsername(user.email);
+                setIsLoggedIn(true);
+            }
+        };
+
+        getUser();
+    }, []);
+
+    const handleLogin = async (email, password) => {
+        let { data, error } = await supabase.auth.signInWithPassword({
+            email: email,
+            password: password,
+        });
+
+        if (error && error.message !== "Auth session missing!") {
+            setAuthError(error.message);
+            return error.message;
+        } else {
+            setUsername(data.user.email);
+            setIsLoggedIn(true);
+            return null;
+        }
+    };
+
+    const handleLogout = async () => {
+        let { error } = await supabase.auth.signOut();
+
+        if (error) {
+            setAuthError(error.message);
+        } else {
+            setIsLoggedIn(false);
+            setUsername("");
+            navigate("/login");
+        }
+    };
     return (
         <div className="container">
-            <Navbar />
+            <Navbar
+                username={username}
+                isLoggedIn={isLoggedIn}
+                onLogout={handleLogout}
+            />
             <Routes>
                 <Route path="/" element={<Welcome />} />
-                <Route path="/login" element={<Login />} />
+                <Route
+                    path="/login"
+                    element={
+                        <ProtectedRoute
+                            isLoggedIn={isLoggedIn}
+                            redirectTo="/now-showing-movies"
+                        >
+                            <Login
+                                username={username}
+                                onLogin={handleLogin}
+                                authError={authError}
+                            />
+                        </ProtectedRoute>
+                    }
+                />
                 <Route path="/now-showing-movies" element={<Home />} />
                 <Route
                     path="/selected-movie"
