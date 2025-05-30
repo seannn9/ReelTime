4f5e8fb227119e59492d174416ae5a6f009abbfa
seannn9 <naeskirlu9@gmail.com>
2025-05-30 14:23:51 +0800

    logout shows confirmation modal

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,44 +1,61 @@
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import logo from "../assets/favicon32.png";
 import "../styles/components/Navbar.css";
+import Modal from "./Modal";
+import { useState } from "react";
 
 export default function Navbar({ username, isLoggedIn, onLogout }) {
     const navigate = useNavigate();
     const location = useLocation();
     const { pathname } = location;
+    const [showModal, setShowModal] = useState(false);
 
     return (
-        <nav className="navbar">
-            <div className="nav-logo">
-                <Link to="/">
-                    <img src={logo} alt="" />
-                    <h1>
-                        Reel
-                        <span className="accent">Time</span>
-                    </h1>
-                </Link>
-            </div>
-            <div className="nav-links">
-                {isLoggedIn ? (
-                    <div className="auth-links">
-                        <p>{username}</p>
-                        <p onClick={onLogout} className="auth-btn">
-                            Logout
-                        </p>
-                    </div>
-                ) : (
-                    pathname !== "/login" && (
+        <>
+            <nav className="navbar">
+                <div className="nav-logo">
+                    <Link to="/">
+                        <img src={logo} alt="" />
+                        <h1>
+                            Reel
+                            <span className="accent">Time</span>
+                        </h1>
+                    </Link>
+                </div>
+                <div className="nav-links">
+                    {isLoggedIn ? (
                         <div className="auth-links">
+                            <p>{username}</p>
                             <p
-                                onClick={() => navigate("/login")}
+                                onClick={() => setShowModal(true)}
                                 className="auth-btn"
                             >
-                                Login
+                                Logout
                             </p>
                         </div>
-                    )
-                )}
-            </div>
-        </nav>
+                    ) : (
+                        pathname !== "/login" && (
+                            <div className="auth-links">
+                                <p
+                                    onClick={() => navigate("/login")}
+                                    className="auth-btn"
+                                >
+                                    Login
+                                </p>
+                            </div>
+                        )
+                    )}
+                </div>
+            </nav>
+            {showModal && (
+                <Modal
+                    action={"Log Out"}
+                    onSubmit={onLogout}
+                    closeModal={() => setShowModal(false)}
+                    headingMsg={"Do you want to log out?"}
+                    subHeadingMsg={"All your unsaved changes will be lost"}
+                />
+            )}
+        </>
     );
 }
